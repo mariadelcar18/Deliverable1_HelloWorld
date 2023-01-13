af69cd6c97572c90467468dd114ff6a833df0f73
mdcpe <mdcpe@LAPTOP-P27RUCJD>
2023-01-13 10:32:12 -0500

    Add project files.

---
+++ Deliverable1_HelloWorld/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Deliverable1_HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Solution (1): Hello, World!");
+            Console.WriteLine(" ");
+
+            // Specifying and assigning stg variables
+            string stg = "Hello, World!";
+            
+            // Writing line 
+            Console.WriteLine("Solution (2): " + stg);
+            Console.WriteLine(" ");
+
+            // Request user input
+            Console.WriteLine("Please enter a user's name: ");
+            string userName = Console.ReadLine();
+            Console.WriteLine("The user's name is: " + userName);
+            Console.WriteLine(" ");
+
+            // Request a numeric data 
+            Console.WriteLine("Please enter the user's age: ");
+            int age = int.Parse(Console.ReadLine());
+            Console.WriteLine("The user's age is: " + age);
+        }
+    }
+}
